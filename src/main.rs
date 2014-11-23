f6e6090a183049c9d1cb82d5a0ff6a330140f5ce
Ian Preston <ian@ian-preston.com>
2014-11-23 13:26:02 -0700

    Map opcodes

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,24 @@ fn execute(memory: &mut CpuState) {
         0xE9 => inst::jmp_word(memory),
         0xEB => inst::jmp_byte(memory),
 
-        0xC6 => inst::mov_modrm(memory),
+        0x88 => inst::mov_modrm_mreg(memory),
+        0x8B => inst::mov_mreg_modrm(memory),
+        0xC6 => inst::mov_modrm_byte(memory),
 
+        0xB0 => inst::mov_reg_byte(memory, AL),
+        0xB1 => inst::mov_reg_byte(memory, CL),
+        0xB2 => inst::mov_reg_byte(memory, DL),
+        0xB3 => inst::mov_reg_byte(memory, BL),
+        0xB4 => inst::mov_reg_byte(memory, AH),
+        0xB5 => inst::mov_reg_byte(memory, CH),
+        0xB6 => inst::mov_reg_byte(memory, DH),
+        0xB7 => inst::mov_reg_byte(memory, BH),
+
+        0xB8 => inst::mov_reg_word(memory, AX),
+        0xB9 => inst::mov_reg_word(memory, CX),
+        0xBA => inst::mov_reg_word(memory, DX),
         0xBB => inst::mov_reg_word(memory, BX),
+        // SP, BP, SI, DI omitted
         0xBF => inst::mov_reg_word(memory, DI),
 
         0x04 => inst::add_reg_byte(memory, AL),
