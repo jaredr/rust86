3a194a8e64809815d5dc96648a91659fd7823807
Ian Preston <hello@ianpreston.io>
2015-01-18 15:34:13 -0700

    Cleanup dump_state

--- src/debugger.rs
+++ src/debugger.rs
@@ -3,7 +3,6 @@ use datatypes::{Byte, Word};
 
 
 pub fn dump_state(cs: &CpuState) {
-    println!("\n*** Begin Processor State Dump ***");
     dump_gr(cs, "ax", Reg16::AX, Reg8::AL, Reg8::AH);
     dump_gr(cs, "bx", Reg16::BX, Reg8::BL, Reg8::BH);
     dump_gr(cs, "cx", Reg16::CX, Reg8::CL, Reg8::CH);
@@ -14,7 +13,6 @@ pub fn dump_state(cs: &CpuState) {
     dump_mem(cs, 0x8030);
     dump_mem(cs, 0x8040);
     dump_mem(cs, 0x8050);
-    println!("*** End Processor State Dump ***");
 }
 
 fn dump_gr(cs: &CpuState, name: &str, x: Reg16, l: Reg8, h: Reg8) {
@@ -32,9 +30,8 @@ fn dump_mem(cs: &CpuState, start: Word) {
     let mut s_chr = String::new();
     for i in range(0, 16) {
         let val: Byte = cs.getmem(start+i);
-        let val_u8: u8 = val.to_u8().unwrap();
         s_hex.push_str(format!("{:0>2X} ", val).as_slice());
-        s_chr.push_str(format!("{:}", val_u8 as char).as_slice());
+        s_chr.push_str(format!("{:}", val as char).as_slice());
     }
     println!("mem    0x{:0>5X} {} {}", start, s_hex, s_chr);
 }
